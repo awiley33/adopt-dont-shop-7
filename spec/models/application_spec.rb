1a8e783abb7ecf9a45ccd4b4fc9ee05c78a225bc
Parker Boeing <parker.boeing@gmail.com>
2023-07-19 11:17:32 -0600

    refactor: Model validations and testing

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,6 +5,16 @@ RSpec.describe Application, type: :model do
     it { should have_many :pet_applications }
     it { should have_many(:pets).through(:pet_applications)}
   end
+  
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:status) }
+  end
 
   describe "instance methods" do
     before(:each) do
