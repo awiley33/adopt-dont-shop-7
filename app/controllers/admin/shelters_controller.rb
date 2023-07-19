7ed5e704a8a4fd1423af7800cdbf63d7c38f5880
Anna Wiley <awiley33@gmail.com>
2023-07-19 09:23:43 -0500

    feat: Add functionality for user story 20

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -2,6 +2,7 @@ class Admin::SheltersController < ApplicationController
   
   def index
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY name DESC")
+    @pending = Shelter.order('name')
   end
 
   def show
